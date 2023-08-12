59ec86ab7c6b67a7fb58a5a3789186cbd35fda14
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2023-08-13 00:32:50 +0530

    Degradation Processes,  Specifications and Summary

---
+++ src/components/FormComponents/FormStepThree.jsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import {
+  COLOMBO,
+  designLifeList,
+  strengthGradeListKurunegala,
+  strengthGradeListColombo,
+} from "../../constants/GeneralConstants";
+
+export default function FormStepTwo({
+  area,
+  designLife,
+  strengthClass,
+  setDesignLife,
+  setStrengthClass,
+}) {
+  const [strengthClassList, setStrengthClassList] = React.useState([]);
+
+  React.useEffect(() => {
+    if (area === COLOMBO) {
+      setStrengthClassList(strengthGradeListColombo);
+    } else {
+      setStrengthClassList(strengthGradeListKurunegala);
+    }
+    console.log("strengthClassList", strengthClassList);
+  }, [area, strengthClassList]);
+
+  const handleDesignLifeChange = (event) => {
+    setDesignLife(event.target.value);
+  };
+
+  const handleStrengthClassChange = (event) => {
+    setStrengthClass(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Specifications
+      </Typography>
+
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Design life</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={designLife || ""}
+              label="Design life"
+              onChange={handleDesignLifeChange}
+              placeholder="Design life"
+            >
+              {designLifeList.map((item) => (
+                <MenuItem key={item.key} value={item.key}>
+                  {item.value}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              Strength Class
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={strengthClass || ""}
+              label="Strength Class"
+              onChange={handleStrengthClassChange}
+              placeholder="Strength Class"
+            >
+              {strengthClassList.map((item) => (
+                <MenuItem
+                  key={item.key}
+                  value={item.key}
+                  disabled={item.disabled}
+                >
+                  {item.value}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
--- src/constants/GeneralConstants.js
+++ src/constants/GeneralConstants.js
@@ -16,3 +16,35 @@ export const KURUNAGALA = "KURUNAGALA";
 export const COLOMBO = "COLOMBO";
 export const INLAND = "INLAND";
 export const MARINE = "MARINE";
+
+export const carbonationClassList = [
+    { key: 'XC2', value: 'XC2', disabled: true },
+];
+
+export const corrosionMarineList = [
+    { key: 'XS1', value: 'XS1', disabled: true },
+    { key: 'XS2', value: 'XS2', disabled: false },
+    { key: 'XS3', value: 'XS3', disabled: true }
+];
+
+export const chemicalAttackInlandList = [
+    { key: 'XA1', value: 'XA1', disabled: false},
+    { key: 'XA2', value: 'XA2', disabled: true },
+    { key: 'XA3', value: 'XA3', disabled: true },
+]
+
+export const designLifeList = [
+    { key: '25', value: '25' },
+    { key: '50', value: '50' },
+    { key: '100', value: '100' }
+]
+
+export const strengthGradeListKurunegala = [
+    { key: 'C25/30', value: 'C25/30' },
+    { key: 'C35/45', value: 'C35/45' },
+    { key: 'C45/55', value: 'C45/55' }
+];
+export const strengthGradeListColombo = [
+    { key: 'C35/45', value: 'C35/45' },
+    { key: 'C45/55', value: 'C45/55' }
+];
